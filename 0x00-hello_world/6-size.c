b5426815a59b3960df3fd8b22548fdfad62f8d9a
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-08 16:02:18 -0700

    updted the latest program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - Entry point
  *
@@ -8,6 +9,7 @@
 int main(void)
 {
 	char c;
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
