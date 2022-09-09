046a6b1bbaf74302fa29dba0be7fcef1146430e8
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-09 13:48:20 -0700

    print numbers 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = o;
+	int i;
+
+	for  (i = 0; i < 10; i++)
+	{
+		printf("%d", c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
