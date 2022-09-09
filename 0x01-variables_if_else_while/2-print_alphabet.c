69c19a2a60d7ed9a8fcd40711f4affb2adb19012
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-09 12:29:21 -0700

    Adding new program

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	int i;
+	for (i=0; i < 26; i++)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
