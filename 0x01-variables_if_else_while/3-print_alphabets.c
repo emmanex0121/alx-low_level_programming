dba12d8a562271f27cc5b9b8418501ece7596418
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-09 12:46:31 -0700

    Alphabet a-z and A-Z using putcgar only

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(c);
+		c++;
+	}
+	for (i = 0; i < 26; i++)
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return(0);
+}
