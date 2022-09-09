46e5e1a775fc37379ca21aa17977bd10175e7ba3
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-09 16:11:47 -0700

    combinations of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	inti i, n1, n2;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (i = i +1; i < 10; i++)
+		{
+			putchar((n1 % 10) + '0');
+			putchar((n2 % 10) + '0');
+
+			if (n1 == 8 && n2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
