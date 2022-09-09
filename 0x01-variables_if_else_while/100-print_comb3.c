472706963e9c0787f9602a587435effe6e61b456
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-09 16:17:05 -0700

    combinations of 2 digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (n1 = 0; n1 < 9; n1++)
 	{
-		for (n2 = n1 +1; n2 < 10; n2++)
+		for (n2 = n1 + 1; n2 < 10; n2++)
 		{
 			putchar((n1 % 10) + '0');
 			putchar((n2 % 10) + '0');
