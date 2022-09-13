b1080b7eb3cbc500534298d7879c037fc24e4fb3
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-13 14:07:01 -0700

    Function that tests for lowe case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+/**
+ * _islower - Function that tests for lower case
+ * @c: parameter that allows function to return integer value
+ *
+ * Description: Case of alphabet is tested
+ */
+
+int _islower(int c)
+{
+	char a ='a';
+	char b = 'A';
+	if (a <= 'z')
+	{
+		return (1);
+	}
+	else if (b <= 'Z')
+	{
+		return (0);
+	}
+}
