a78c1cec236c001762b6984d3e4890b955eb55e4
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-13 10:39:05 -0700

    Function to print alphabets in lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -13,7 +13,7 @@ int main(void)
 	return (0);
 }
 
-void print_alphabet(void)
+void print_alphabet()
 {
 	char a = 'a';
 	int i;
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,6 @@
 
 void _putchars(int n, char var[]);
 
-void print_alphabet(void);
+void print_alphabet();
 
 
