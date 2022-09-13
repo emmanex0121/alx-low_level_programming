27d312b5002664ae33c9c62ad121731bdd3cccc4
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-13 16:13:05 -0700

    Function that tests sign of numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -25,5 +25,4 @@ int print_sign(int c)
 		return (-1);
 		putchar ('-');
 	}
-
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,7 +11,7 @@ int _islower(int c);
 
 void _putchar(int in);
 
-int _isalpha(int c)
+int _isalpha(int c);
 	
-int print_sign(int n);;
+int print_sign(int n);
 
