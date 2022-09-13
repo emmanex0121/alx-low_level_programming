140fc0d81e2fea4cb64d7505004da3c040cee3fe
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-13 16:28:23 -0700

    Function that tests sign of numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int c)
 {
 	if (c > 0)
 	{
-		return (1);
 		putchar ('+');
+		return (1);
 	}
 	else if (c == 0)
 	{
-		return (0);
 		putchar ('0');
+		return (0);
 	}
 	else if (c < 0)
 	{
-		return (-1);
 		putchar ('-');
+		return (-1);
 	}
 }
