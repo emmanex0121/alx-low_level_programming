af7d55f5322288a9de5fa66500f7f60e5e5b792e
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-13 18:05:37 -0700

    updates

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,19 @@
  * Return: Lorem Ipsium
  */
 
-int print_sign(int c)
+int print_sign(int n)
 {
-	if (c > 0)
+	if (n > 0)
 	{
 		_putchar ('+');
 		return (1);
 	}
-	else if (c < 0)
+	else if (n < 0)
 	{
 		_putchar ('-');
 		return (-1);
 	}
-	else if (c == 0)
+	else if (n == 0)
 	{
 		_putchar ('0');
 		return (0);
