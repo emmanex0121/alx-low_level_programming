79d0c2a54477927714634c0339137fef47a20b96
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-13 20:17:10 -0700

    Function to print natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,16 +2,36 @@
 
 
 /**
+ * print_to_98 - Function that prints value to and from 98
+ * @n: Parameter n
  *
+ * Description: Lorem Ipsum
+ * Return: 0 Success
  */
 
 void print_to_98(int n)
 {
 	unsigned int i;
-	for (i = n; i < 99; i++)
+
+	if (n < 98)
 	{
-		printf("%d", n);
-		n++;
+		for (i = n; i < 98; i++)
+		{
+			printf("%d", n);
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d", n);
+			n--;
+		}
+	}
+	else if (n == 98)
+	{
+		printf("%d", 98);
 	}
 	putchar('\n');
 }
