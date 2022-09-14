f1941f9bc9d1c5752285b6239e314e89eb4e9892
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-13 18:03:30 -0700

    updates

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,18 +13,17 @@ int print_sign(int c)
 {
 	if (c > 0)
 	{
-		putchar ('+');
+		_putchar ('+');
 		return (1);
 	}
 	else if (c < 0)
 	{
-		putchar ('-');
+		_putchar ('-');
 		return (-1);
 	}
 	else if (c == 0)
 	{
-		putchar ('0');
+		_putchar ('0');
 		return (0);
 	}
-	putchar('\n');
 }
