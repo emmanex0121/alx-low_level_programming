171105790268072858b3028144cf5c1299382551
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-13 18:16:24 -0700

    Function to return absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * _abs - function that returns absolute value of an int
+ */
+
+int _abs(int)
+{
+	n = (n * n) / n;
+	return (n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+int _abs(int);
 
 void _putchars(int n, char var[]);
 
