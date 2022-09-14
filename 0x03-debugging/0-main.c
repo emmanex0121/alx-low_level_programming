1c2cfe49a87128132c62ea3a8fc919e846d5ed10
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-14 05:26:20 -0700

    positive and negative test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - positiveor negative test 
+ */
+
+int main(void)
+{
+	int i = 0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/**
+ *
+ */
+
+void positive_or_negative(int i);
+
+
