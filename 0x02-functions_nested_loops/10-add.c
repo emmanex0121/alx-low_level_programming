b536610c4615fd8098273800d1bee19a54152ad1
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-13 19:50:05 -0700

    function to add integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio>
+
+
+/**
+ * add - function to add two integers
+ * @a: parameter a
+ * @b: parameter b
+ *
+ * Description: Lorem Ipsum
+ * Retun: 0 success
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
