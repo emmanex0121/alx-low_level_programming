a4204b2705a0451fbbd0104193b6480afebc1253
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-15 13:45:15 +0000

    Function to test upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - Tests case
+ * @c: integer c
+ */
+
+int _isupper(int c)
+{
+	c = 'A';
+	int i;
+
+	if (i = c; i <= 'Z'; i++)
+	{
+		return (1);
+	}
+	else{
+		return (0);
+	}
+}
