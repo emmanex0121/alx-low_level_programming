6308cdc6d28dad968deecbe463043e6d24c0c8d2
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-15 16:15:52 +0000

    Funtion to omit some numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_most_numbers - Function to print all numbers 0 - 9 except 2 & 4
+ *
+ * Description: Lorem Ipsum
+ */
+
+void print_most_numbers(void)
+{
+	int a = '0';
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (a == '2' && a == '4')
+		{
+		}
+		else
+		{
+		_putchar(a);
+		}
+		a++;
+	}
+	putchar('\n');
+}
