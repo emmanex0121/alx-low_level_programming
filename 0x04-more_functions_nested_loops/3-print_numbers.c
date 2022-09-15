5107304ffd9ce1f4fc7437c81d6ec666a66b9f00
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-15 15:21:51 +0000

    Function to print numbers from 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_numbers - Function to print all numbers from 0 - 9
+ *
+ * Description: Lorem Ipsum
+ */
+
+void print_numbers(void)
+{
+	int a = '0'
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(a);
+		a++;
+	}
+}
