9970bb87e1ab2d9042734d7436cc233ddb10630f
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-16 01:07:33 +0000

    Function to print integer using putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,7 +7,15 @@
 
 void print_number(int n)
 {
-	if (n / 10)
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+		print_number(n);
+	}
+	else if (n / 10)
+	{
 		print_number(n / 10);
-	putchar((n % 10) + '0');
+		putchar((n % 10) + '0');
+	}
 }
