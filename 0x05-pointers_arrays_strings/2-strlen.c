cb63e0ad68832497994b8b4a7678830d97f8fc4f
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-19 22:44:03 +0000

    Function to print string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * int _strlen - Function to print string lenght
+ * @*s: Parameter for the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	while (*(s + i))
+	{
+		i++;
+		return (i);
+	}
+}
