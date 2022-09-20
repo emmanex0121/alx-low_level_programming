ad8a9d84152261400bd78e7f1ada6810d95e6ec3
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-20 02:10:03 +0000

    Function to print strings in 2-s increment

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 /**
+ * _strlen - Function to return string lenght
+ * @s: String pointer
+ *
+ * Return: String lenght
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+
+/**
  * puts2 - Function that prints in 2's increment
  * @str: pointer to the string
  */
@@ -9,11 +27,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (*str != '\0')
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
 	{
-		i += 2;
-		str += 2;
 		putchar(str[i]);
 	}
+
 	putchar('\n');
 }
