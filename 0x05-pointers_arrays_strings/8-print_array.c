bee0bbbbc956d042fcf2ef342d50133eb26f826a
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-20 03:27:04 +0000

    Function to print array elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_array - Function that prints array elements
+ * @a: pointer paramerter
+ * @n: integer parameter
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d, ", a[i]);
+	}
+		printf("\n");
+}
