2a07f5569e00a47bbc4ce9560121332e631442c7
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-21 17:48:38 +0000

    Function to concatenate strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * _strcat - Function to concatenate strings
+ * @dest: parameter for string 1
+ * @src: parameter for string 2
+ *
+ * Return: contatenated string stored in @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int lenght, j;
+
+	while (dest[lenght] != '\0')
+	{
+		lenght++;
+	}
+	
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[lenght] = src[j];
+		lenght++;
+	}
+	return (dest);
+}
