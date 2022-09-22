e37a3df93043e102eb968e95578abf5d82d691fa
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-22 18:34:46 +0000

    Function to reverse elements of array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * reverse_array - Function to reverse elements of an array
+ * @a: parameter for array
+ * @n: Parameter for array lenght
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = n; i >= 0; i--)
+	{
+		if (i == 0)
+		{
+			printf("%d", a[i]);
+		}
+		else
+			printf("%d, ", a[i]);
+	}
+	printf("\n");
+}
