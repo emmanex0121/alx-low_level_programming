56cb0fd5c7dc599855797fb0000b90f8f5e40cdf
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-23 01:03:30 +0000

    Function to reverse elements of array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -25,9 +25,9 @@ void reverse_array(int *a, int n)
 /*	} */
 		for (j = i + 1; j > 0; j--)
 		{
-			temp = a[j];
-			a[j] = a[j - 1];
-			a[j - 1] = temp;
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
 		}
 	}
 }
