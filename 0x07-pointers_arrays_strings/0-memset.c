86392b966abe31a0c09de7555ad95056d181f6b0
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-26 20:45:53 +0000

    Function to replace n number of bytes from a location

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,6 +4,9 @@
  * _memset - Function to replace memory area with a constant
  * @s: parameter of the pointer to memory area
  * @n: parameter to hold number of bytes to alter
+ * @b: parameter to hold the constant
+ *
+ * Return: the swapped values
  */
 
 char *_memset(char *s, char b, unsigned int n)
