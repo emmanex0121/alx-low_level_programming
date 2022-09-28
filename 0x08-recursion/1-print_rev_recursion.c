426c68376d0743568fb06a67344e8f257c47eb4f
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-28 16:55:01 +0000

    Reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: The string to print
+ *
+ * Return: Void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+}
