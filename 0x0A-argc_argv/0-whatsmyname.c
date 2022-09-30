8e1b45a540c1ddec942762e4215c48fd5f4485ec
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-09-30 20:35:31 +0000

    'Update'

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  *
  * Return: 0 for no errors 1 otherwise
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%s\n", argv[0]);
 	return (0);
