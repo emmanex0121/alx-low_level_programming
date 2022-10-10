255032ae5ed1f0a3a169bd1e17145ac1a15b9a88
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-10-10 18:00:03 +0000

    Struct dog with dog attributes

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - dog struct with attributes
+ * @name: Dog name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
