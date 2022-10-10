cc55b3655973127dc964e39dcd2c45a4f8aa90c4
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-10-10 22:47:46 +0000

    New dog struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,12 +11,38 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog;
+	unisigned int nl, ol, i;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
 		return (NULL);
-	ndog->name = name;
+	for (nl = 0; name[nl]; nl++)
+		;
+	nl++;
+	ndog->name = malloc(n1 * sizeof(char));
+	if (ndog-> == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
+	for (i = 0; i < nl; i++)
+		ndog->name[i] = name[i];
+
 	ndog->age = age;
-	ndog->owner = owner;
+
+	for (ol = 0; owner[ol]; ol++)
+		;
+	ol++;
+	ndog->owner = malloc(sizeof(char));
+	if (ndog->owner == NULL)
+	{
+		free(ndog->name);
+		free(ndog);
+		return (NULL);
+	}
+	for (i = 0; owner[i]; i++)
+		ndog->owner[i] = owner[i];
 	return (ndog);
 }
