decf238e4f963a1bfa76f7fbc19d27d8927eb3eb
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-10-10 21:18:08 +0000

    New dog struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - new dog struct inside the typedef of dog_t
+ * @name: new dog's name
+ * @age: new dog's age
+ * @owner: New dog's owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ndog;
+
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+		return (NULL);
+	ndog->name = name;
+	ndog->age = age;
+	ndog->owner = owner;
+	return (ndog);
+}
