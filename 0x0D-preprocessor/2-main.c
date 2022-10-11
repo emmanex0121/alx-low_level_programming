26ae40d06d9a9c1a33698de9fe2700a602b19e66
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-10-11 21:30:30 +0000

    Print name of the file compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the name of the compiled program
+ * @argc: number of command line arguments
+ * @argv: program command line argumanets
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
