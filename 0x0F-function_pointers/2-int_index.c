f91a311ec3748314e7b5d4e1ca4f10b29ba53fa9
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-10-13 02:18:58 +0000

    Function that searches for a number

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -23,8 +23,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 		for (i = 0; i < size; i++)
 			if (cmp(array[i]))
 				return (i);
-			else
-				return (-1);
 	}
-	/*return (-1);*/
+	return (-1);
 }
