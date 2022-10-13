183622c10a7036a0ebff72cc1de1fb677aca5150
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-10-13 20:31:09 +0000

    Function that sums all its parameters

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS
-#define VARIDIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
+
+/*#include <stdarg.h>*/
+/*#include <stdio.h>*/
 
 int sum_them_all(const unsigned int n, ...);
 
