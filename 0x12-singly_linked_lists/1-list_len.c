28d6493773ee79f3f16ba3503692ddc2f5478168
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-10-21 02:24:30 +0000

    Function to return number of elements in the list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/*
+ * list_len - Function that returns number of elements in a list
+ * @h: pointer to the list
+ *
+ * Return: umber of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
