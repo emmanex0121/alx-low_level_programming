5a30b705eb33f9b0ae868dd045a6ea4fc0ba1f6c
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-10-21 04:35:48 +0000

    Function to free list heaps

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,11 +7,12 @@
 
 void free_list(list_t *head)
 {
-	/*list_t *current;*/
-	while (head != NULL)
+	list_t *current;
+
+	while ((current = head) != NULL)
 	{
 		head = head->next;
-		free(head->str);
-		free(head);
+		free(current->str);
+		free(current);
 	}
 }
