6b8476e9ecd874d7ec08ebc96a6495cd0ab9d6f5
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-10-21 21:47:12 +0000

    Function that executes before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,9 +2,10 @@
 
 /**
  * bef_main - Function that executes before the main
+ * Return: Nothing
  */
 
-(void) __attribute__ ((constructor)) bef_main()
+void __attribute__ ((constructor)) bef_main()
 {
 	printf("You're beat! and yet, you must allow,
 			\nI bore my house upon my back!\n");
