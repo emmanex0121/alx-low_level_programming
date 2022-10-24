0dfa43776dd9e2dfe72ca6a695398a2a66965469
Codeinx <aseyeged@gmail.com>
2022-10-25 00:57:16 +0100

    Function to print elements of a list

---
+++ 0x13-more_singly_linked_lists/test/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - Function to print list elements and retun number of elements
+ * @h: struct holding the singly linked list
+ *
+ * Return: Number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t num_node = 0;
+
+	while (h)
+	{
+		if (h->n)
+			printf("%d", h->n);
+		num_node++;
+		h = h->next;
+	}
+	return (num_node);
+}
---
+++ 0x13-more_singly_linked_lists/test/README.md
@@ -0,0 +1,1 @@
+All about 0x13-more_singly_linked_lists
