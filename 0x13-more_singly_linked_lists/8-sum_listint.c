53439889075052b176495a7d81e6d4d6c76bb561
Codeinx <aseyeged@gmail.com>
2022-10-25 01:53:00 +0100

    Function to sum all data n

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Function to return linked list lenght
+ * @head: Singly Linked list head pointer
+ *
+ * Return: Sum of all the data n
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum_data = 0;
+
+	while (head)
+	{
+		if (h->n)
+			sum_data += h->n;
+		h = h->next;
+	}
+	return (sum_data;
+}
