795a718adcd952b47037538568963ae5c6b4d01d
heep-jay <jubrilmuritala69@gmail.com>
2022-10-25 21:44:24 -0500

    reverses the list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -25,6 +25,7 @@ int pop_listint(listint_t **head);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
+listint_t *reverse_listint(listint_t **head);
 
 
 #endif /* LISTS_H */
