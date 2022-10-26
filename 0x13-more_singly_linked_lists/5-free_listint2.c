c6d0221b40d9f45fd5c81c01cb03a8f7b458db5e
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-10-26 01:50:12 +0000

    Frees lists and sets head to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	/*if (head == NULL)*/
-		/*return;*/
+	if (head == NULL)
+		return;
 
 	while (*head)
 	{
 		temp = *head;
 		*head = (*head)->next;
-		(*head)->next = NULL;
 		free(temp);
 	}
 	head = NULL;
