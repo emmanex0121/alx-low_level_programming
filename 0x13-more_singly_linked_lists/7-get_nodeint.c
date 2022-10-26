ffa561835cf5940a8acecbb680b3faf1596fdfa9
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-10-26 16:56:19 -0700

    Get the nth index node of the list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Function to get nth index node of the list
+ * @head: singly linked list
+ * @index: index of the node to search for
+ *
+ * Return: NULL if node at nth index is NULL, else nth node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (i = 0; head->next; i++)
+	{
+		if (index == i)
+			if (head != NULL)
+				return (head);
+			else
+				return (NULL);
+		head = head->next;
+	}
+}
