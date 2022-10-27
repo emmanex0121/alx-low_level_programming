986ddb47f925b6638992bab72cf32517558804bc
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-10-26 18:01:10 -0700

    Get the node at nth index of list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,8 +12,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
+	/*if (head == NULL)*/
+		/*return (NULL);*/
 
 	for (i = 0; head->next; i++)
 	{
