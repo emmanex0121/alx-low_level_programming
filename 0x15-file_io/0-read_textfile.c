c6c99c8525623ac190ba3990d129248dc85a15d7
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-11-01 02:27:57 +0000

    reads text file and prints to POSTFIX stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
+	buf = malloc(sizeof(letters); /* letters); */
 	if (buf == NULL)
 	{
 		close(fd);
