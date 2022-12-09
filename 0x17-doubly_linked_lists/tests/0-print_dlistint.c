79805cb04cc3301134c7caacfaa28314b613638f
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-12-09 11:55:41 -0500

    Prints elements in the list and return no of nodes

---
+++ 0x17-doubly_linked_lists/tests/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_dlistint - Function that prints the nodes of a D-L-List
+ * @h: Double Linked List passed in
+ *
+ * Return: Number of nodes in the D-L-List
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t num_elem;
+
+	while(h)
+	{
+		if (h->n)
+		{
+			printf("%d\n", h->n);
+			num_elem++;
+			h = h->next;
+		}
+	}
+	return (num_elem);
+}
