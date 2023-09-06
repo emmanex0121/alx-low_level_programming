e462652030c95d7ab8976be188c43aa370318322
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-09-06 19:41:50 +0100

    task_1 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <fcntl.h>
-#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,6 @@
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
 
 #endif /* MAIN_H */
