85742c5a7a72c784c3c8749bbdf0566633629bd5
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-09-07 16:26:59 +0100

    TASK_4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,6 +5,8 @@
 
 /**
  * main - program that copies content from one file to another
+ * @argc: number of arguments
+ * @argv: argument vectors
  *
  * Return: 0 on success
  */
@@ -27,7 +29,6 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd1 = open(argv[2], O_CREAT | O_WRONLY, 0664);
 	if (fd1 == -1)
 	{
@@ -38,14 +39,12 @@ int main(int argc, char **argv)
 	buf = malloc(sizeof(char) * 1024);
 	read(fd, buf, 1024);
 	write(fd1, buf, 1024);
-
 	fd_close = close(fd);
 	if (fd_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_close);
 		exit(100);
 	}
-
 	fd1_close = close(fd1);
 	if (fd1_close == -1)
 	{
@@ -55,4 +54,3 @@ int main(int argc, char **argv)
 
 	return (0);
 }
-
