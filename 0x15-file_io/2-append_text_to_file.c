9da336ada8175461891a2273ee2548251814ca1a
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-09-07 14:24:37 +0100

    Task_3 2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends a text to a file
+ * @filename: filename
+ * text_content: null terminated string content to be appended
+ *
+ * Return: 1 if file exists, -1 if file not exist | no write permission
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, count;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		write(fd, "", 0);
+	}
+	else
+	{
+		for (count = 0; text_content[count]; count++)
+			;
+		write(fd, text_content, count);
+	}
+
+	return (1);
+}
