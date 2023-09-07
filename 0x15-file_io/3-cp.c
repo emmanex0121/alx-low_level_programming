29b1c118c7e986a137898dbdf3351a9d9278ff99
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-09-07 16:53:16 +0100

    corrections

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,6 +4,50 @@
 #include <unistd.h>
 
 /**
+ * fd_open_err - checks if files can be opened.
+ * @fd: file_from.
+ * @fd1: file_to.
+ * @argv: arguments vector.
+ *
+ * Return: no return.
+ */
+void fd_open_err(int fd, int fd1, char **argv)
+{
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * fd_close_err - checks if files can be closed.
+ * @fd_close: file_from.
+ * @fd1_close: file_to.
+ * @argv: arguments vector.
+ *
+ * Return: no return.
+ */
+void fd_close_err(int fd_close, int fd1_close, char **argv)
+{
+	if (fd_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_close);
+		exit(100);
+	}
+	if (fd1_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1_close);
+		exit(100);
+	}
+}
+
+/**
  * main - program that copies content from one file to another
  * @argc: number of arguments
  * @argv: argument vectors
@@ -14,43 +58,25 @@
 int main(int argc, char **argv)
 {
 	int fd, fd_close, fd1, fd1_close;
-	/*int count;*/
 	char *buf;
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	fd = open(argv[1], O_RDONLY | O_TRUNC);
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	fd1 = open(argv[2], O_CREAT | O_WRONLY, 0664);
-	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+	fd = open(argv[1], O_RDONLY);
+	fd1 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd_open_err(fd, fd1);
 
 	buf = malloc(sizeof(char) * 1024);
 	read(fd, buf, 1024);
 	write(fd1, buf, 1024);
+
 	fd_close = close(fd);
-	if (fd_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_close);
-		exit(100);
-	}
 	fd1_close = close(fd1);
-	if (fd1_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1_close);
-		exit(100);
-	}
+	fd_close_err(fd_close, fd1_close);
 
 	return (0);
 }
