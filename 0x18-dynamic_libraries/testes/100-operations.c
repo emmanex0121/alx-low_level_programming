ec4b28e2bded5b3865b34ab56228e11fe157ec71
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-12-13 18:19:16 +0100

    shell script - dynamic lib

---
+++ 0x18-dynamic_libraries/testes/100-operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * mul - function to return multiplication of two numbers
+ * @a: value 1
+ * @b: value 2
+ *
+ * Return: the multiplication of two values
+ */
+int mul(int a, int b)
+{
+	return a * b;
+}
+
+/**
+ * add - function to add two numbers
+ * @a: value 1
+ * @b: value 2
+ *
+ * Return: the addition  of two values
+ */
+int add(int a, int b)
+{
+        return a + b;
+}
+
+/**
+ * sub - function to add two numbers
+ * @a: value 1
+ * @b: value 2
+ *
+ * Return: the subtraction of two values
+ */
+int sub(int a, int b)
+{
+        return a - b;
+}
+
+/**
+ * div - function to divide two numbers
+ * @a: value 1
+ * @b: value 2
+ *
+ * Return: the division of two values
+ */
+int div(int a, int b)
+{
+        return a / b;
+}
+
+/**
+ * div - function to find the modulus between two numbers
+ * @a: value 1
+ * @b: value 2
+ *
+ * Return: the modulus of two values
+ */
+int mod(int a, int b)
+{
+        return a % b;
+}
